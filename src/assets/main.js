2943a75fe14774bfd84efad04c29c10b3ffda7ae
Dwight <carter91jr@gmail.com>
2017-08-17 13:15:58 -0400

    Corrected ajax

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,8 +1,10 @@
 $(function() {
+
 $.ajax({
-  dataType:'jsonp';
-  url:'https://www.codeschool.com/users/2818593';
+  dataType:'jsonp',
+  url:'https://www.codeschool.com/users/2818593.json',
   success:function(response){
+    console.log('response', response);
 
   }
 })
