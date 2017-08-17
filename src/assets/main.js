df1cb123f03a720c8402fc3a825aa659478ba0f5
Dwight <carter91jr@gmail.com>
2017-08-17 13:49:20 -0400

    Final changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,11 +1,40 @@
 $(function() {
 
-$.ajax({
-  dataType:'jsonp',
-  url:'https://www.codeschool.com/users/2818593.json',
-  success:function(response){
-    console.log('response', response);
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2818593.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      populateWithCourses(data.courses.completed)
+    }
+  });
+
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
 
   }
-})
+
 });
